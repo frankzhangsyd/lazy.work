d6b9fbb8155523b190b62ad28f006bfb0307b143
frankzhang21 <644379641@qq.com>
2020-05-14 18:42:02 +1000

    First function Woooahah

---
+++ R/alteryx.R
@@ -0,0 +1,16 @@
+#' Generate formula strings for Alteryx
+#'
+#' Genetate a string which can be copied into Alteryx formula tool
+#' @param x a vecotr of column names in Alteryx
+#' @param f the function will be applied across columns
+#' @return a single string which can be copied into Alteryx formula
+#' @export
+#' @examples
+#' fields <- c("a", "b", "c", "d")
+#' ayx_fl(fields,"+")
+
+ayx_fl <- function(x,f=c("+","-","*","/")){
+  stopifnot(is.character(x))
+  f <- match.arg(f)
+  paste(paste0("[",x,"]"),collapse = f)
+}
